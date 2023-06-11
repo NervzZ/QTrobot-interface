ea16221277085522a89d0f9190c55a2745a835b3
NervzZ <julien.jordan@epfl.ch>
2023-06-11 16:15:31 +0200

    able to list online files and download them

--- src/pages/Visualisation/Visualisation.jsx
+++ src/pages/Visualisation/Visualisation.jsx
@@ -1,10 +1,96 @@
 import 'SRC/App.css'
+import {FormControl, InputLabel, MenuItem, Select} from "@mui/material"
+import {useEffect, useState} from "react"
+import {Download} from "@mui/icons-material"
+import Button from "@mui/material/Button"
+import {getDownloadURL, listAll, ref} from "firebase/storage"
+import {storage} from "SRC/firebaseConfig.js"
 
 
 const Visualisation = () => {
+
+    const [file, setFile] = useState('')
+    const [format, setFormat] = useState('.pdf')
+    const [fileRefs, setFileRefs] = useState({})
+    const [refresh, setRefresh] = useState(true)
+
+    useEffect(() => {
+        const listAllRef = ref(storage, 'commandOutputs');
+        listAll(listAllRef)
+            .then((res) => {
+                const promises = res.items.map((item) =>
+                    getDownloadURL(ref(storage, item.fullPath)).then((url) => {
+                        return { [item.name]: url };
+                    })
+                );
+                return Promise.all(promises);
+            })
+            .then((urlObjs) => {
+                const urls = Object.assign({}, ...urlObjs);
+                setFileRefs(urls);
+            });
+    }, []);
+
+    const handleFileChange = (event) => {
+        setFile(event.target.value)
+    }
+
+    const handleFormatChange = (event) => {
+        setFormat(event.target.value)
+    }
+
+    const handleDownload = () => {
+        if (file !== '') {
+            //required to mimick a link click to start the download
+            const link = document.createElement('a');
+            link.href = file;
+            document.body.appendChild(link); // Required for Firefox
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
+
     return (
-        <div>
-            <h1>{Visualisation.name} Work in progress!</h1>
+        <div className="App" style={{
+            maxWidth: '950px',
+            margin: '50px auto',
+            textAlign: 'left',
+        }}>
+            <FormControl sx={{m: 1, minWidth: 350}}>
+                <InputLabel id="demo-simple-select-helper-label">Online files</InputLabel>
+                <Select
+                    labelId="demo-simple-select-helper-label"
+                    id="demo-simple-select-helper"
+                    value={file}
+                    label="Online files"
+                    onChange={handleFileChange}
+                >
+                    {Object.entries(fileRefs).map(([file, url]) => (
+                        <MenuItem key={file} value={url}>{file}</MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+            <FormControl sx={{m: 1, minWidth: 100}}>
+                <InputLabel id="demo-simple-select-helper-label">format</InputLabel>
+                <Select
+                    labelId="demo-simple-select-helper-label"
+                    id="demo-simple-select-helper"
+                    value={format}
+                    label="Format"
+                    onChange={handleFormatChange}
+                >
+                    <MenuItem value={'.pdf'}>.pdf</MenuItem>
+                    <MenuItem value={'.csv'}>.csv</MenuItem>
+                </Select>
+            </FormControl>
+            <Button
+                sx={{mt: 2, ml: 2}}
+                variant="outlined"
+                startIcon={<Download/>}
+                onClick={handleDownload}
+            >
+                Download
+            </Button>
         </div>
     )
 }
