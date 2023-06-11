e63b31ca83b07e07a24375077adb717bbbdccc49
NervzZ <56892303+NervzZ@users.noreply.github.com>
2023-06-11 06:42:27 +0200

    feature/run-terminal (#13)

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import {exec} from 'child_process'
+import cors from 'cors'
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+app.post('/run-command', (req, res) => {
+    const command = req.body.command;
+    const commandPrefix = command.split(' ')[0];
+
+    if (commandPrefix !== 'rosrun' && commandPrefix !== 'roslaunch') {
+        return res.status(400).json({ error: 'Invalid command.' });
+    }
+
+    /**
+     * TODO - for testing purposes we are using "ls" as the command to execute as it's an available command on Ubuntu.
+     * The final implementation for the end product will simply use the command constant defined above which will be
+     * either a ros
+     */
+    exec('ls', (error, stdout, stderr) => {
+        if (error) {
+            res.status(500).json({ error: `error: ${error.message}` });
+            return;
+        }
+        if (stderr) {
+            res.status(500).json({ error: `stderr: ${stderr}` });
+            return;
+        }
+        res.status(200).json({ response: stdout });
+    });
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
